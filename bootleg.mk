386101e236a4be7341d30dc5fd6ad43da92bc294
Jens Lody <jens@jenslody.de>
2020-03-25 13:08:22 +0100

    msm8916-common: bringup of device-individuation for Bootleggers
    Signed-off-by: Jens Lody <jens@jenslody.de>

---
+++ bootleg.mk
@@ -0,0 +1,14 @@
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+
+# Include full languages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
---
+++ bootleg_j5x.mk
@@ -0,0 +1,5 @@
+# Release name
+PRODUCT_RELEASE_NAME := Samsung Galaxy J5 2016
+
+$(call inherit-product, device/samsung/msm8916-common/bootleg.mk)
+
