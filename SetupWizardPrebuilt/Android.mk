59e2f806998de5e3953a60d6fce20381403f4b17
Arbab(AB) <arbabaliabforever@gmail.com>
2021-04-29 20:52:55 +0500

    Update Android.mk

--- SetupWizardPrebuilt/Android.mk
+++ SetupWizardPrebuilt/Android.mk
@@ -19,7 +19,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := SetupWizardPrebuilt-arm
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := prebuilt/SetupWizardPrebuilt.apk
+LOCAL_SRC_FILES := SetupWizardPrebuilt.apk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
